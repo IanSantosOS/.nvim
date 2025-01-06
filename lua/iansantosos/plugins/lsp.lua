ced07279e5a6b697136006fd252d81a4cae8eb79
IanSantosOS <iann.silva2006@gmail.com>
2025-01-06 10:35:36 -0300

    fix new tsserver name + update plugin versions

--- lua/iansantosos/plugins/lsp.lua
+++ lua/iansantosos/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "tsserver",
+                "ts_ls",
                 "html",
                 "cssls",
                 "emmet_ls",
