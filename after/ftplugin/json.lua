36a771bd3d65fc66d99e3a6870abac9efd2e1149
IanSantosOS <iann.silva2006@gmail.com>
2025-03-15 17:00:39 -0300

    ftplugin: update css, html and json comments; lazy-lock: update plugins

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -1,6 +1,6 @@
 local set = vim.opt_local
 
-set.tabstop = 2      -- Define que 1 Tab = 4 Espaços
+set.tabstop = 2      -- Define que 1 Tab = 2 Espaços
 set.softtabstop = 0  -- Os espaços inseridos quando aperta Tab ou BackSpace será o mesmo do tabstop
 set.shiftwidth = 2   -- Espaços inseridos ao utilizar a tecla de recuo
 set.expandtab = true -- Transforma Tab em Espaços
